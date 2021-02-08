22a919315af176cc38ec93c2ba9f052c8b2a6cba
Dmitry Medyannik <mail@artdima.ru>
2021-02-08 10:42:32 +0300

    Add test project

---
+++ LingvanexAPI/LingvanexAPI.swift
@@ -0,0 +1,122 @@
+//
+//  LingvanexAPI.swift
+//  LingvanexAPI
+//
+//  Created by Medyannik Dmitri on 07.02.2021.
+//
+
+import Foundation
+import UIKit
+
+public struct Translate: Codable {
+    /// the text of the error. It is null if the response status is 200. Otherwise, it contains a string
+    public let err: String?
+    /// result of translation. In the event that a line was sent to the translation, the result is also a string; if an array of strings, then we also get an array of strings
+    public let result: String
+    /// the number of letters translated using the Lingvanex service cache
+    public let cacheUse: Int
+    /// source data for translation
+    public let source: String
+    /// code of the source language in the format “language code”. In the case of sending a translation of an array of strings with different language content, only the language of the first element of the array is returned
+    public let from: String
+    /// transliteration of source data. In the event that a line was sent to the translation, the result is also a string; if an array of strings, then we also get an array of strings
+    public let sourceTransliteration: String
+    /// transliteration results. In the event that a line was sent to the translation, the result is also a string; if an array of strings, then we also get an array of strings
+    public let targetTransliteration: String
+}
+
+/// A helper class for using Lingvanex API
+public class LingvanexAPI {
+    
+    /// Shared instance.
+    public static let shared = LingvanexAPI()
+    
+    /// API structure.
+    private struct API {
+        /// Base Lingvanex API url.
+        static let base = "https://api-b2b.backenster.com/b1/api/v3"
+        
+        /// Getting the list of languages
+        struct getLanguages {
+            static let method = "GET"
+            static let url = API.base + "/getLanguages"
+        }
+        
+        /// Text translation
+        struct translate {
+            static let method = "POST"
+            static let url = API.base + "/translate"
+        }
+    }
+    
+    /// API key.
+    private var apiKey: String!
+    /// Default URL session.
+    private let session = URLSession(configuration: .default)
+    
+    /**
+        Initialization.
+    
+        - Parameters:
+            - apiKey: A valid API key to handle requests for this API. Authentication of requests is done by adding the “Authorization” header with the following data format: Bearer The key can be created on the user control panel page https://lingvanex.com/account.
+    */
+    public func start(with apiKey: String) {
+        self.apiKey = apiKey
+    }
+    
+    /**
+        Translates input text, returning translated text.
+    
+        - Parameters:
+            - from: The language code in the format “language code_code of the country” from which the text is translated. The language code is represented only in lowercase letters, the country code only in uppercase letters (example en_GB, es_ES, ru_RU and etc.). If this parameter is not present, the auto-detect language mode is enabled
+            - to:  Language code in the format “language code_code of the country” to which the text is translated (required)
+            - data: Data for translation (required). Two types of data are supported: a string and an array of strings
+            - platform: api
+    */
+    //public func translate(_ q: String, _ target: String, _ source: String, _ format: String = "text", _ model: String = "base", _ completion: @escaping ((_ text: String?, _ error: Error?) -> Void)) {
+    public func translate(_ from: String, _ to: String, _ data: String, _ platform: String = "api", _ completion: @escaping ((_ translate: Translate?, _ error: Error?) -> Void)) {
+        guard var urlComponents = URLComponents(string: API.translate.url) else {
+            completion(nil, nil)
+            return
+        }
+        
+        var queryItems = [URLQueryItem]()
+        queryItems.append(URLQueryItem(name: "key", value: apiKey))
+        queryItems.append(URLQueryItem(name: "from", value: from))
+        queryItems.append(URLQueryItem(name: "to", value: to))
+        queryItems.append(URLQueryItem(name: "data", value: data))
+        queryItems.append(URLQueryItem(name: "platform", value: platform))
+        urlComponents.queryItems = queryItems
+        
+        guard let url = urlComponents.url else {
+            completion(nil, nil)
+            return
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = API.translate.method
+        
+        let task = session.dataTask(with: urlRequest) { (data, response, error) in
+            guard let data = data,                                // is there data
+                  let response = response as? HTTPURLResponse,    // is there HTTP response
+                  (200 ..< 300) ~= response.statusCode,           // is statusCode 2XX
+                  error == nil else {                                // was there no error, otherwise ...
+                
+                completion(nil, error)
+                return
+            }
+            
+            guard let object = try? JSONDecoder.decode(Translate.self, from: data),
+                  let d = object["data"] as? [String: Any] else {
+                
+                completion(nil, error)
+                return
+            }
+            
+            completion(translatedText, nil)
+        }
+        task.resume()
+    }
+    
+    
+}
